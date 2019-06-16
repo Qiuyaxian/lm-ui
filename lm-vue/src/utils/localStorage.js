c99ce299687373baaa1959119348b3554a419394
YGT <33387460+YGT-cxy@users.noreply.github.com>
2019-06-16 23:12:44 +0800

    Update localStorage.js
    修改name为storageKey的命名，name应该是保留字，不能使用；修改里面的部分if else 结构为三元运算符，constructor应该采用es6的解构赋值方式传入参数，但是没必要这样使用，直接调用静态方法反而更好，不需要new对象

--- lm-vue/src/utils/localStorage.js
+++ lm-vue/src/utils/localStorage.js
@@ -4,155 +4,153 @@ import { stateHandle } from './tools.js'
  * @type {Object}
  */
 export default class LocalStorage {
-  constructor(options = { method:'get', name:'', value:'', callback: null  }) { 
-  	if(LocalStorage.check()){
-	  if(options['method'] == 'getSync'){
-	    if(options['callback']) options['callback'](LocalStorage[options['method']](options['name']))
-	    else return LocalStorage[options['method']](options['name'])
-	  }else{
-	    return LocalStorage[options['method']](options['name'])	
-	  }
-	}
+  // constructor(options = { method: 'get', name: '', value: '', callback: null }) {
+  //   if (LocalStorage.check()) {
+  //     if (options['method'] == 'getSync') {
+  //       if (options['callback']) options['callback'](LocalStorage[options['method']](options['name']))
+  //       else return LocalStorage[options['method']](options['name'])
+  //     } else {
+  //       return LocalStorage[options['method']](options['name'])
+  //     }
+  //   }
+  // }
+  // 使用es6的解构赋值
+  constructor({ method = 'get', storageKey = '', callback }) {
+    if (this.check()) {
+      // 应该使用全等符号，采用严格模式的写法
+      if (method === 'getSync') { // 同步执行
+        if (callback) {
+          callback(this[method](storageKey))
+        } else {
+          return this[method](storageKey)
+        }
+      } else { // 异步执行
+        return this[method](storageKey)
+      }
+    }
   }
   /**
    * [check 是否支持localStorage]
    * @return {[type]} [description]
    */
   static check() {
-	if(window && window.localStorage) return true;
-	else return false;
+    return window && window.localStorage ? true : false;
   }
   /**
    * [get 异步获取localStorage值]
-   * @param  {[type]} name [description]
-   * @return {[type]}      [description]
+   * @param  {[type]} storageKey key
+   * @return {[type]}      value
    */
-  static get(name){
-	let _this = this;
-	return new Promise((resolve,reject)=>{
-      if(_this.getSync(name)){
-        resolve(_this.getSync(name));
-      }else{
-        reject(null);
-      }
-	}).catch( err => err); 
+  static get(storageKey) {
+    return new Promise((resolve, reject) => {
+      const value = this.getSync(storageKey);
+      value ? resolve(value) : reject(null);
+    });
   }
   /**
    * [getSync 同步获取localStorage值]
-   * @param  {[type]} name [description]
+   * @param  {[type]} storageKey key
+   * @return {[type]}      value
+   */
+  static getSync(storageKey) {
+    if (!storageKey) return null; // 应该返回的是null
+    if (this.check()) { // this 指向 LocatStorate
+      const value = localStorage.getItem(storageKey);
+      // 利用三元运算符解决单行的内容块代码会较为简洁方便
+      return value ? /^\{.*?\}$/gi.test(value) ? JSON.parse(value) : value : null;
+    } else {
+      stateHandle('不支持localStorage'); // 这里采用new Error返回一个错误较为合适，又或者在check时便提示
+      return null;
+    }
+  }
+  /**
+   * [set 异步设置localStorage值]
+   * @param {[type]} storageKey  [description]
+   * @param {[type]} value [description]
+   */
+  static set(storageKey, value) {
+    return new Promise((resolve, reject) => {
+      this.setSync(storageKey, value, (value) => {
+        resolve(value);
+      });
+    });
+  }
+  /**
+   * [setSync 同步设置localStorage值]
+   * @param {[type]}   storageKey     [description]
+   * @param {[type]}   value    [description]
+   * @param {Function} callback [description]
+   */
+  static setSync(storageKey, value, callback) {
+    if (!storageKey || !value) return false;
+
+    if (this.check()) {
+      typeof value === 'string'
+        ? localStorage.setItem(storageKey, value)
+        : localStorage.setItem(storageKey, JSON.stringify(value))
+
+      // 直接判断是否存在回调函数，存在则回调传入当前设置的value
+      callback && callback(value);
+      // if (callback) {
+      //   if (this.getSync(name) != null) {
+      //     callback && callback(value);
+      //   } else {
+      //     stateHandle('null指针异常');
+      //   }
+      // } else {
+      //   if (this.getSync(name) != null) {
+      //     return new Promise((resolve, reject) => {
+      //       if (!this.getSync(name)) {
+      //         reject()
+      //       } else {
+      //         resolve(this.getSync(name));
+      //       }
+      //     }).catch(err => err);
+      //   } else return null;
+      // }
+    } else {
+      stateHandle('不支持LocalStorage')
+    };
+  }
+  /**
+   * [del 异步删除localStorage值]
+   * @param  {[type]} storageKey key
    * @return {[type]}      [description]
    */
-  static getSync(name) {
-    if(!name) return false;
-    if(LocalStorage.check()){
-      if( localStorage.getItem(name) ){
-        if( /^\{.*?\}$/gi.test( localStorage.getItem(name) ) )
-          return JSON.parse(localStorage.getItem(name));
-        else
-          return localStorage.getItem(name); 
-      }else return null;
-    }else stateHandle('不支持localStorage');
-   }
-   /**
-    * [set 异步设置localStorage值]
-    * @param {[type]} name  [description]
-    * @param {[type]} value [description]
-    */
-   static set(name,value){
-	 let _this = this;
-	 return new Promise( ( resolve, reject) => {
-	   _this.setSync(name,value,(value)=>{
-	     resolve(value);
-	   });
-	 }).catch( err => err); 
-   }
-   /**
-    * [setSync 同步获取localStorage值]
-    * @param {[type]}   name     [description]
-    * @param {[type]}   value    [description]
-    * @param {Function} callback [description]
-    */
-   static setSync(name,value, callback) {
-	 if(!name) return false;
-	 if(!value) return false;
-	 if(LocalStorage.check()){  
-	   if(typeof(value) == 'string')
-	     localStorage.setItem( name, value );
-	   else
-	     localStorage.setItem( name, JSON.stringify(value) );
-	   if(callback) {
-	     if(this.getSync(name) != null){
-	       callback && callback(value);
-	     }else{
-	       stateHandle('null指针异常');
-	     }
-	   }else{
-	   	 if(this.getSync(name) != null){
-   	 	   return new Promise( (resolve,reject) => {
-		     if(!this.getSync(name)){
-		       reject()
-		     }else{
-		       resolve(this.getSync(name));
-		     }
-		   }).catch( err => err);
-	   	 } 
-	     else return null;
-	   }
-	 }else{
-	   stateHandle('不支持LocalStorage')
-	 };
-   }
-   /**
-    * [del 异步删除localStorage值]
-    * @param  {[type]} name [description]
-    * @return {[type]}      [description]
-    */
-   static del(name){
-	 let _this = this;
-	 return new Promise( (resolve,reject) => {
-	   _this.delSync(name);
-	   if(_this.getSync(name)){
-	     reject()
-	   }else{
-	     resolve(null);
-	   }
-	 }).catch( err => err); 
-   }
-   /**
-    * [delSync 同步获取localStorage值]
-    * @param  {[type]} name [description]
-    * @return {[type]}      [description]
-    */
-   static delSync(name){
-	 if( !name ) return false;
-	 if(LocalStorage.check()) 
-	   localStorage.removeItem(name);
-	 else 
-	   stateHandle('不支持LocalStorage');
-   }
-   /**
-    * [clear 异步清除所以localStorage值]
-    * @return {[type]} [description]
-    */
-   static clear(){
-	 let _this = this;
-	 return new Promise( ( resolve, reject) => {
-	   _this.clearSync();
-	   resolve(null);
-	 }).catch( err => err); 
-   } 
-   /**
-    * [clearSync 同步清除localStorage值]
-    * @return {[type]} [description]
-    */
-   static clearSync() {
-	 if(LocalStorage.check())
-	   localStorage.clear(); 
-	 else 
-	   stateHandle('不支持LocalStorage');
-   }
-} 
-  
-  
-  
-  
+  static del(storageKey) {
+    return new Promise((resolve, reject) => {
+      this.delSync(storageKey);
+      if (this.getSync(storageKey)) {
+        reject()
+      } else {
+        resolve(null);
+      }
+    });
+  }
+  /**
+   * [delSync 同步删除localStorage值]
+   * @param  {[type]} storageKey [description]
+   * @return {[type]}      [description]
+   */
+  static delSync(storageKey) {
+    if (!storageKey) return false;
+    this.check() ? localStorage.removeItem(storageKey) : stateHandle('不支持LocalStorage');
+  }
+  /**
+   * [clear 异步清除所以localStorage值]
+   * @return {[type]} [description]
+   */
+  static clear() {
+    return new Promise((resolve, reject) => {
+      this.clearSync();
+      resolve(null);
+    });
+  }
+  /**
+   * [clearSync 同步清除localStorage值]
+   * @return {[type]} [description]
+   */
+  static clearSync() {
+    this.check() ? localStorage.clear() : stateHandle('不支持LocalStorage');
+  }
+}
